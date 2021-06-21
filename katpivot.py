86ec8dbbc9bde569f832eb2bc6f31b49b5e9dad7
David Schrittesser <david@logic.univie.ac.at>
2021-06-20 23:49:33 -0400

    normalization in stacked barchart

--- katpivot.py
+++ katpivot.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 
 def get_article_names(data):
-    return sorted(set(data["name"]))
+    return get_values(data,"name")
 
 def get_values(data,column):
     return sorted(set(data[column]))
@@ -37,7 +37,7 @@ def make_barchart(data, target, selection = None, features = ["h1","h2","h3"], f
     values = []
    
     for feature in features: 
-        normalized_target_value = (data [data[feature_col]==feature][target]/data [data[feature_col]==feature]["Impressions"]).sum()
+        normalized_target_value = (data [data[feature_col]==feature][target]).sum()/(data [data[feature_col]==feature]["Impressions"]).sum()
         values.append(normalized_target_value)
     #pdb.set_trace()
     #sort values and features
@@ -50,6 +50,13 @@ def make_barchart(data, target, selection = None, features = ["h1","h2","h3"], f
     plt.ylabel(target + " per impressions")
 
 def get_normalized_values(data, target, features = ["h1","h2","h3"], feature_col="hypothesis"):
+    values = []
+    for feature in features: 
+        normalized_target_value = (data [data[feature_col]==feature][target]).sum()/(data [data[feature_col]==feature]["Impressions"]).sum()
+        values.append(normalized_target_value)
+    return np.array(values) 
+
+def get_normalized_valuesBROKE(data, target, features = ["h1","h2","h3"], feature_col="hypothesis"):
     values = []
     for feature in features: 
         normalized_target_value = (data [data[feature_col]==feature][target]/data [data[feature_col]==feature]["Impressions"]).sum()
@@ -69,6 +76,7 @@ def make_multi_barchart(data, targets, selection, features = ["h1","h2","h3"], t
     plt.ylabel("video watches per impressions")
     
     
+    
 def make_stacked_barchart(data, target, stack_column, feature_column, title=""):
     features = get_values(data,feature_column)
     selection_stack = get_values(data,stack_column)
@@ -76,7 +84,7 @@ def make_stacked_barchart(data, target, stack_column, feature_column, title=""):
     for stack_selection in selection_stack:
         #print(stack_selection)
         view = data[(data[stack_column]==stack_selection)]
-        values = get_normalized_values(view,target,features,feature_col="name")
+        values = get_normalized_valuesBROKE(view,target,features,feature_col="name")
         #print(values)
         #values = 1
         plt.bar(features, values, label=stack_selection, bottom = previous_values)
@@ -85,6 +93,35 @@ def make_stacked_barchart(data, target, stack_column, feature_column, title=""):
     plt.legend()
     plt.ylabel(target)
     plt.xticks(rotation=90)
+    
+def make_stacked_barchart2(data, target, stack_column, feature_column, title=""):
+    features = get_values(data,feature_column)
+    split_by_values = get_values(data,stack_column)
+    
+    
+    
+
+    previous_values=0
+    for stack in split_by_values:
+        values = []
+        for feature in features: 
+            selection = (data[feature_column]==feature) & (data[stack_column]==stack)
+            normalized_target_value = (data [selection][target]).sum()/(data [data[feature_column]==feature]["Impressions"]).sum()
+            values.append(normalized_target_value)
+        values = np.array(values)
+        plt.bar(features, values, label=stack, bottom = previous_values)
+        previous_values += values
+    plt.title(title)
+    plt.legend()
+    plt.ylabel(target)
+    plt.xticks(rotation=90)
+    
+def get_normalized_values(data, target, features = ["h1","h2","h3"], feature_col="hypothesis"):
+    values = []
+    for feature in features: 
+        normalized_target_value = (data [data[feature_col]==feature][target]).sum()/(data [data[feature_col]==feature]["Impressions"]).sum()
+        values.append(normalized_target_value)
+    return np.array(values) 
  
 def make_multi_histogram(data, target, features = ["h1","h2","h3"], title=None):
     if not title:
